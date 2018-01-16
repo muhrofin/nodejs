7dc05562d99163346642b7b8d5d55c9957af72bc
muhrofin <muhrofin@jagokomputer.com>
2018-01-16 09:18:47 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,44 @@
+var socket  = require( 'socket.io' );
+var express = require('express');
+var app     = express();
+var server  = require('http').createServer(app);
+var io      = socket.listen( server );
+var port    = process.env.PORT || 3002;
+
+server.listen(port, function () {
+  console.log('Server listening at port %d', port);
+});
+
+
+io.on('connection', function (socket) {
+
+  socket.on( 'new_count_message', function( data ) {
+    io.sockets.emit( 'new_count_message', { 
+    	new_count_message: data.new_count_message
+
+    });
+  });
+
+  socket.on( 'update_count_message', function( data ) {
+    io.sockets.emit( 'update_count_message', {
+    	update_count_message: data.update_count_message 
+    });
+  });
+
+  socket.on( 'new_message', function( data ) {
+    io.sockets.emit( 'new_message', {
+		
+		to_id: data.to_id,
+		from_id: data.from_id,
+		message: data.message,
+		created_at: data.created_at,
+		id: data.id,
+		username: data.username,
+		title: data.title,
+		by: data.by,
+		last_id: data.last_id
+    });
+  });
+
+  
+});
